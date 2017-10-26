9adfb5f8046be14b510f5118fa5f353ecdd29eda
adryssonlima <adrysson.lima@gmail.com>
2017-10-26 19:42:03 -0300

    Lista de categorias

--- app/src/main/java/com/example/adrysson/mymoney/CategoriaActivity.java
+++ app/src/main/java/com/example/adrysson/mymoney/CategoriaActivity.java
@@ -1,13 +1,20 @@
 package com.example.adrysson.mymoney;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 
 public class CategoriaActivity extends AppCompatActivity {
+//    implements OnItemClickListener
+//    String lista[] = {"Cheese", "Pepperoni", "Black Olives"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,34 @@ public class CategoriaActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(CategoriaActivity.this);
+                builder.setTitle("Nova Categoria");
+
+// Set up the input
+                final EditText input = new EditText(CategoriaActivity.this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+// Set up the buttons
+                builder.setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String m_Text = input.getText().toString();
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
